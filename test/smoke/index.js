87f717a546ad428c20c5fffc1062cbc4994445dd
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2020-02-08 22:32:14 +0800

    test(test): 添加单元测试
    :skit

---
+++ test/smoke/index.js
@@ -0,0 +1,5 @@
+const Mocha = require('mocha');
+
+const mocha = new Mocha({
+  timeout: '10000ms',
+});
---
+++ test/unit/webpack-base-test.js
@@ -0,0 +1,5 @@
+const assert = require('assert');
+
+describe('webpack.base.js test case', () => {
+  const baseConfig = require('../../lib/webpack.base');
+});
